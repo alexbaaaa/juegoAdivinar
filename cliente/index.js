10a12b17ff6895f1e2bff878ef22d28aaa5c247c
alexbaaaa <aalejandrodesarrollo@gmail.com>
2024-04-10 15:52:41 +0200

    Implementacion del ui en index.js

--- cliente/index.js
+++ cliente/index.js
@@ -1,7 +1,24 @@
-let webSocket = new WebSocket('ws://localhost:8080');
+import {Comunicacion} from "./modules/comunicacion.js";
+// Esta variable la estraeremos en otra clase
+let ui = {
+    listo : () => {
+      document.getElementById('estado').innerHTML = 'Conectado';
+    },
+    empieza : () => {
+        document.getElementById('botonEmpezar').style.display = 'block';
+        document.getElementById('botonEmpezar').onclick = function() {
+          Comunicacion.comenzar();
+        }
+    },
+    prepararJuego : () => {
+      document.getElementById('botonEmpezar').style.display = 'none';
+      document.getElementById('juego').style.display = 'block';
+      document.getElementById('enviar').onclick = () => {
+        Comunicacion.enviarNumero(document.getElementById('numero').value);
+      };
+    }
+}
+let comunicacion = new Comunicacion('localhost',8080,ui);
+document.getElementById('estado').innerHTML = 'Esperando...';
 
-webSocket.onmessage = (event) => {
-    console.log(event.data);
-    
-  };
   
